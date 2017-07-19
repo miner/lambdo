bec403f69d3bee6a2e59908d5736701253ff387f
Steve Miner <steveminer@gmail.com>
2017-07-19 10:54:29 -0400

    bump lmdbjava

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.taoensso/nippy "2.13.0"]
-                 [org.lmdbjava/lmdbjava "0.5.0"]])
+                 [org.lmdbjava/lmdbjava "0.6.0"]])
 
 
